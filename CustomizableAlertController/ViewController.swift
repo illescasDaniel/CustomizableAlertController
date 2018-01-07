de966637293ddbfc7140cf6d7f37072eda600e12
illescasDaniel <illescas.daniel@protonmail.com>
2018-01-07 18:17:46 +0100

    Initial Commit

---
+++ CustomizableAlertController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomizableAlertController
+//
+//  Created by Daniel Illescas Romero on 07/01/2018.
+//  Copyright © 2018 Daniel Illescas Romero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
